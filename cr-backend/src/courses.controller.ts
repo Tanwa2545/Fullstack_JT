4570f91b3faa60d9055018e7ffa2279500842110
Tanwa2545 <goopkus@gmail.com>
2024-08-25 23:11:33 +0700

    Edit CourseController message

--- cr-backend/src/courses.controller.ts
+++ cr-backend/src/courses.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class CoursesController {
   @Get()
   findAll() {
-    return {message: 'Hello world'};
+    return {message: 'Hello human'};
   }
 }
